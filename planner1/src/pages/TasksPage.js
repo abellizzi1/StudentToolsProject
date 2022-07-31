4956a7c6540cd835b8dcf1bd73b260f2131127dd
Angelo Bellizzi <abelliz734@gmail.com>
2022-07-30 21:44:39 -0500

    Changes to TasksPage

--- planner1/src/pages/TasksPage.js
+++ planner1/src/pages/TasksPage.js
@@ -15,11 +15,21 @@ const TasksPage = ({ handleSetCurrentPage }) => {
             <div className='content'>
                 <Link to={"/tasks/create-task"}>
                     <button 
-                    onClick={addNote} 
                     className='addTaskButton'>
                     {<FaIcons.FaPen />} Create Task
                     </button>
                 </Link>
+
+                <div>
+                    <div className='task'>
+                        <h1 className='taskTitle'>
+                            Title
+                        </h1>
+                        <p className='taskText'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
             </div>
     )
 }
