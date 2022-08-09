7e9c22714a935d3e67e28188a41d3bbd81cb834e
abellizzi1 <abelliz734@gmail.com>
2022-08-09 14:27:50 -0500

    Can now add friends to a group task, group task creation almost complete

--- planner1/src/components/CreateGroupTaskFriend.js
+++ planner1/src/components/CreateGroupTaskFriend.js
@@ -3,11 +3,24 @@ import { useState } from 'react';
 import {Link} from "react-router-dom";
 import '../pages/GroupTasksPage.css';
 
-const CreateGroupTaskFriend = ({  }) => {
-	
+const CreateGroupTaskFriend = ({ friendName, friendEmail, handleAddToGroup, handleRemoveFromGroup }) => {
+
+    const [isAdded, setIsAdded] = useState(false);
+
+    const handleClick = () => {
+        if (isAdded) {
+            setIsAdded(false);
+            handleRemoveFromGroup(friendEmail);
+        }
+        else {
+            setIsAdded(true);
+            handleAddToGroup(friendEmail);
+        }
+    }
+
     return (
 		<div>
-			
+			<button onClick={() => { handleClick() } } className={`groupTasksFriendButton${isAdded ? "Selected" : ""}`}>{friendName}</button>
 		</div>
 	);
 };
--- planner1/src/pages/CreateGroupTaskPage.js
+++ planner1/src/pages/CreateGroupTaskPage.js
@@ -4,16 +4,100 @@ import { nanoid } from 'nanoid';
 import Note from '../components/Note.js';
 import * as FaIcons from 'react-icons/fa';
 import {Link} from "react-router-dom";
+import CreateGroupTaskFriend from '../components/CreateGroupTaskFriend';
+import axios from 'axios'
 
 const CreateGroupTaskPage = () => {
-
-
     /*
     On the right side of the screen:
     Show all friends in a list of buttons.
     On Click, the button turns from blue to green and the friend is added to the group task.
     If Clicked while green, turns back to blue and the friend is removed from the group task.
     */
+
+    const [friendsRepo, setFriendsRepo] = useState([]);
+    const [usersRepo, setUsersRepo] = useState([]);
+    const [friends, setFriends] = useState([]);
+    const [groupTaskMembers, setGroupTaskMembers] = useState([]);
+    const [runCount, setRunCount] = useState(0);
+
+    const getRepo = () => {
+
+        axios.get('http://localhost:4000/app/users/get')
+            .then((response) => {
+                const tempUsersRepo = response.data;
+                console.log(tempUsersRepo);
+                setUsersRepo(tempUsersRepo);
+            });
+
+        axios.get('http://localhost:4000/app/friends/get')
+            .then((response) => {
+                const tempFriendsRepo = response.data;
+                console.log(tempFriendsRepo);
+                setFriendsRepo(tempFriendsRepo);
+            });
+       
+        }
+
+        const setFriendsState = () => {
+            var loginEmail = localStorage.getItem('loggedInEmail');
+            console.log(localStorage.getItem('loggedInEmail'));
+            console.log(friendsRepo);
+            var friendsArrayTemp = [];
+            for (let i = 0; i < friendsRepo.length-1; i++) {
+                for (let j = i+1; j < friendsRepo.length; j++) {
+                    if (friendsRepo[i].receiver === friendsRepo[j].sender 
+                    && friendsRepo[i].sender === friendsRepo[j].receiver
+                    && (friendsRepo[i].sender === loginEmail || friendsRepo[j].sender === loginEmail)) {
+                        console.log('friend found');
+                        var friendToAdd;
+                        if (friendsRepo[i].sender === loginEmail)
+                        {
+                            friendToAdd = usersRepo.filter((user) => user.email === friendsRepo[i].receiver);
+                        }
+                        else {
+                            friendToAdd = usersRepo.filter((user) => user.email === friendsRepo[i].sender);
+                        }
+    
+                        if (friendToAdd.length > 0) {
+                            const newFriend = {
+                                name: (friendToAdd[0].firstName + " " + friendToAdd[0].lastName),
+                                email: friendToAdd[0].email
+                            };
+                            friendsArrayTemp.push(newFriend);
+                        }
+                    }
+                }
+            }
+            setFriends(friendsArrayTemp);
+        }
+
+        const addToGroupTask = (email) => {
+            const newGroupTaskMembers = [...groupTaskMembers, email];
+            setGroupTaskMembers(newGroupTaskMembers);
+        }
+
+        const removeFromGroupTask = (email) => {
+            var newGroupTaskMembers = groupTaskMembers;
+            for (let i = 0; i < newGroupTaskMembers.length; i++) {
+                if (newGroupTaskMembers[i] === email) {
+                    newGroupTaskMembers.splice(i, 1);
+                }
+            }
+            setGroupTaskMembers(newGroupTaskMembers);
+        }
+
+        useEffect(() => {
+        
+            if (friends.length === 0 && runCount < 5 && localStorage.getItem('loggedInEmail') !== '') {
+                getRepo();
+                setFriendsState();
+                setRunCount(runCount + 1);
+            }
+            
+        }, [friendsRepo]);
+
+
 
     return(
         <div className='content'>
@@ -22,15 +106,15 @@ const CreateGroupTaskPage = () => {
                     <h1 className='groupTaskInformationHeader'>Group Task Information</h1>
                     <h2 className='groupTaskFriendsHeader'>Click on a friend to share the task with them</h2>
                     <div className='createTaskFriendsContent'>
-                        <button className='groupTasksFriendButtonSelected'>Angelo Belrrinnnnni</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
-                        <button className='groupTasksFriendButton'>Joe</button>
+                        
+                        {friends.map((friend) => (
+                            <CreateGroupTaskFriend
+                                friendName={friend.name}
+                                friendEmail={friend.email} 
+                                handleAddToGroup={addToGroupTask}
+                                handleRemoveFromGroup={removeFromGroupTask}
+                            />
+                        ))}
                     </div>
 
                     <textarea
